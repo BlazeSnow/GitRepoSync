1164ea52dd51990a53c5fdfe8b68835e9a204e57
BlazeSnow <git@blazesnow.org>
2026-09-18 15:58:48 +0800

    搭建前端工程骨架（Vite + React + TypeScript + Tailwind CSS）

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: false,
+  },
+  envPrefix: ["VITE_", "TAURI_ENV_"],
+  build: {
+    target: "chrome105",
+    minify: "esbuild",
+    sourcemap: false,
+  },
+});
